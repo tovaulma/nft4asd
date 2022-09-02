f6f94799b912eeec9ba28ff5b3a82bfd50e8f207
William Park <william02px2022@gmail.com>
2022-09-02 18:26:34 +0900

    bio structure

--- src/components/WhoWeAre.jsx
+++ src/components/WhoWeAre.jsx
@@ -11,7 +11,25 @@ const WhoWeAre = () => {
             <div className='bio'>
                 <img src={Px1} alt="person"/>
                 <div className='content'>
-                    
+                    <h2>Sewon Park</h2>
+                    <h3>Project Lead & Founder</h3>
+                    <h4>Minim laboris qui occaecat nisi est. Eu irure esse reprehenderit est in. Duis qui ea cupidatat anim aliqua adipisicing. Non elit anim et velit adipisicing culpa. Duis ea fugiat sint laboris aliqua mollit velit. Ullamco nostrud laboris aute non tempor non in officia duis elit cillum. Irure consequat nisi tempor officia laborum esse ex ea.</h4>
+                </div>
+            </div>
+            <div className='bio'>
+                <img src={Px1} alt="person"/>
+                <div className='content'>
+                    <h2>Ryan Zhao</h2>
+                    <h3>Head of Blockchain</h3>
+                    <h4>Minim laboris qui occaecat nisi est. Eu irure esse reprehenderit est in. Duis qui ea cupidatat anim aliqua adipisicing. Non elit anim et velit adipisicing culpa. Duis ea fugiat sint laboris aliqua mollit velit. Ullamco nostrud laboris aute non tempor non in officia duis elit cillum. Irure consequat nisi tempor officia laborum esse ex ea.</h4>
+                </div>
+            </div>
+            <div className='bio'>
+                <img src={Px1} alt="person"/>
+                <div className='content'>
+                    <h2>Gio Huh</h2>
+                    <h3>Head of Technology</h3>
+                    <h4>Minim laboris qui occaecat nisi est. Eu irure esse reprehenderit est in. Duis qui ea cupidatat anim aliqua adipisicing. Non elit anim et velit adipisicing culpa. Duis ea fugiat sint laboris aliqua mollit velit. Ullamco nostrud laboris aute non tempor non in officia duis elit cillum. Irure consequat nisi tempor officia laborum esse ex ea.</h4>
                 </div>
             </div>
         </div>
