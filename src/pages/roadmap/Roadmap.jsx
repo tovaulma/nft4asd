1fc5cc05a92678c6a4e4194f2843c1642a0e2e0f
William Park <william02px2022@gmail.com>
2022-09-02 17:17:09 +0900

    header

--- src/pages/roadmap/Roadmap.jsx
+++ src/pages/roadmap/Roadmap.jsx
@@ -1,10 +1,15 @@
 import './roadmap.css'
+import Header from '../../components/Header'
 import Timeline from '../../components/Timeline'
 import Footer from '../../components/Footer'
+import Image from '../../images/header__image.jpg'
 
 const Roadmap = () => {
   return (
     <>
+    <Header title="Roadmap" image={Image}>
+      Culpa Lorem incididunt consequat eiusmod aute.
+    </Header>
     <Timeline/>
     <Footer/>
     </>
